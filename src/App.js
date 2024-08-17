ebb8ae09bf7ec6f9fd334cfe900379b69bd33165
Malik's MacBook Pro <bismillah@Maliks-MacBook-Pro.local>
2024-08-18 00:08:30 +0500

    new commit

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Jenkins on Udemy
+          Learn Jenkins by MALIK MUNEEB ASGHAR - The Logic Lounge
         </a>
       </header>
       <p>
